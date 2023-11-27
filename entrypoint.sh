462035f6db1184a4dc6449ed4f75a0ee5cabb593
Shea Phillips <shea.phillips@gmail.com>
2023-11-27 13:46:27 -0800

    DEVX-911
    Adding files to enable testing.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,20 @@
 #!/bin/bash -l
 
+
+ACTION=$1
+echo "Action: $ACTION"
+
 source ~/.virtualenvs/techdocs/bin/activate
 cd /src
-techdocs-cli build --verbose --no-docker
+
+if [ $ACTION == "build" ]
+then
+	echo "Building TechDocs from Markdown..."
+	techdocs-cli build --verbose --no-docker
+elif [ $ACTION == "publish" ]
+then
+	echo "Publishing TechDocs to DevHub..."
+	techdocs-cli publish --verbose --no-docker
+fi
+
+

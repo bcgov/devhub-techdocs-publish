0011d6295c7257b94777472686ff2a27c552d463
Shea Phillips <shea.phillips@gmail.com>
2023-11-28 12:11:21 -0800

    DEVX-911
    enabling publish stage

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,12 +31,12 @@ then
 	echo "Publishing TechDocs to DevHub..."
 	echo "Bucket (secret): $TECHDOCS_S3_BUCKET_NAME"
 	echo "Dev root path(var): $TECHDOCS_S3_DEV_ROOT_PATH"
-#	techdocs-cli publish --publisher-type awsS3 \
-#                --storage-name $TECHDOCS_S3_BUCKET_NAME \
-#                --entity $ENTITY_NAMESPACE/$ENTITY_KIND/$ENTITY_NAME \
-#                --awsEndpoint $AWS_ENDPOINT \
-#                --awsS3ForcePathStyle true \
-#                --awsBucketRootPath $TECHDOCS_S3_DEV_ROOT_PATH
+	techdocs-cli publish --publisher-type awsS3 \
+                --storage-name $TECHDOCS_S3_BUCKET_NAME \
+                --entity $ENTITY_NAMESPACE/$ENTITY_KIND/$ENTITY_NAME \
+                --awsEndpoint $AWS_ENDPOINT \
+                --awsS3ForcePathStyle true \
+                --awsBucketRootPath $TECHDOCS_S3_DEV_ROOT_PATH
 fi
 
 

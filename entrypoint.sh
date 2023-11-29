60e9540f6ece605e62d6556b113eb038d34a4981
Shea Phillips <shea.phillips@gmail.com>
2023-11-29 14:48:17 -0800

    DEVX-911
    Confirming values are set as expected

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,20 +23,43 @@ export AWS_ACCESS_KEY_ID="$INPUT_S3_ACCESS_KEY_ID"
 export AWS_SECRET_ACCESS_KEY="$INPUT_S3_SECRET_ACCESS_KEY"
 export AWS_DEFAULT_REGION="$INPUT_S3_REGION"
 
+ENTITY_PATH="$ENTITY_NAMESPACE/$ENTITY_KIND/$ENTITY_NAME"
+
 echo "Building TechDocs from Markdown for entity '$ENTITY_NAMESPACE/$ENTITY_KIND/$ENTITY_NAME'"
 techdocs-cli build --verbose --no-docker
 
 if [ "$INPUT_PUBLISH" == "true" ]
 then
 	echo "Publishing TechDocs to DevHub..."
-	echo "Bucket (secret): $TECHDOCS_S3_BUCKET_NAME"
+
+	echo "Bucket : $TECHDOCS_S3_BUCKET_NAME"
 	echo "Dev root path(var): $TECHDOCS_S3_DEV_ROOT_PATH"
+	echo "Entity path: $ENTITY_PATH"
+    echo "Endpoint: $AWS_ENDPOINT"
+    echo "Region: $AWS_DEFAULT_REGION"
+
+    if [ -z "$AWS_ACCESS_KEY_ID" ]
+    then
+    	echo "AWS_ACCESS_KEY_ID is NOT set!"
+
+	else
+		echo "AWS_ACCESS_KEY_ID is set!"
+	fi
+
+	if [ -z "$AWS_SECRET_ACCESS_KEY" ]
+	then
+		echo "AWS_SECRET_ACCESS_KEY is NOT set!"
+
+	else
+		echo "AWS_SECRET_ACCESS_KEY is set!"
+	fi
+
 	techdocs-cli publish --publisher-type awsS3 \
-                --storage-name $TECHDOCS_S3_BUCKET_NAME \
-                --entity $ENTITY_NAMESPACE/$ENTITY_KIND/$ENTITY_NAME \
-                --awsEndpoint $AWS_ENDPOINT \
+                --storage-name "$TECHDOCS_S3_BUCKET_NAME" \
+                --entity "$ENTITY_PATH" \
+                --awsEndpoint "$AWS_ENDPOINT" \
                 --awsS3ForcePathStyle true \
-                --awsBucketRootPath $TECHDOCS_S3_DEV_ROOT_PATH
+                --awsBucketRootPath "$TECHDOCS_S3_DEV_ROOT_PATH"
 fi
 
 

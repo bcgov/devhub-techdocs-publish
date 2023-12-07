1fdc448bb0872172f457693a4610cc431adddd94
Shea Phillips <shea.phillips@gmail.com>
2023-12-06 19:00:09 -0800

    DEVX-911
    update .gitignore

---
+++ tag.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -l
+
+
+usage() {
+	echo "usage: tag.sh <tag_name>"
+	exit 1
+}
+
+exitWithMessage() {
+	echo $1
+
+	if [ $# -eq 2 ]
+      then
+        exit $2
+    else
+    	exit 1
+    fi
+}
+
+
+if [[ `git status --porcelain` ]]; then
+	exitWithMessage "There are local changes. Please revert or commit these before running this command."
+fi
+
+if [ $# -eq 0 ]
+  then
+    usage
+else
+	TAG=$1
+fi
+
+
+read -p "Updating action file to use new Docker tag '${TAG}'. Press any key to continue..."
+cat <<< "$(yq -y '.runs.image = "docker://ghcr.io/bcgov/devhub-techdocs-publish:$TAG"' action.yml)" > action.yml
+
+#git commit -m "Updated action.yml to use a new Docker image with tag '${TAG}'."
+#git push
+#
+#git tag -a -m "adding version tag $TAG" $TAG
+#git push --follow-tags
+
+
